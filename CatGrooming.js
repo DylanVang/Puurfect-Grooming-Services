3d2e476e3dbf883936f04316c77987396c365b43
Dylan Vang <144581049+DylanVang@users.noreply.github.com>
2023-10-15 15:03:51 -0700

    Add files via upload

---
+++ CatGrooming.js
@@ -0,0 +1,26 @@
+
+
+mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {  if (document.body.scrollTop > 20  || document.documentElement.scrollTop > 20) {
+mybutton.style.display = "block";
+} else {
+mybutton.style.display = "none";
+}
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE     and Opera
+}
+
+
+
+
+myButton.addEventListener('click', () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
